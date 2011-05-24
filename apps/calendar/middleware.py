c131e43c7ed074b2645318a64e322f3669077f5f
Ross Karchner <rosskarchner@gmail.com>
2011-05-23 20:43:45 -0400

    tweaking performance variables (cron and queues), removed some vestigal Tipfy code

--- apps/calendar/middleware.py
+++
@@ -1,6 +0,0 @@
-from tipfy import RequestHandler, Response, redirect, cached_property
-
-class SiteRequiredMiddleware(object):
-	def pre_dispatch(self, handler):
-		if not handler.site:
-			return Response("I don't know that site!")
--- subscriptions/urls.py
+++ subscriptions/urls.py
@@ -4,10 +4,7 @@ from tasks import subscribe_email, start_schedule_newsletters, schedule_next_new
 urlpatterns = patterns('subscriptions.views',
       url(r'^new/$', 'new_subscription', name="new_subscription"),
       url(r'^subscribe_email/$',subscribe_email),
-      url(r'^start_schedule_newsletters/$', start_schedule_newsletters),
-      url(r'^create_and_send/$', create_and_send_campaign),
-      url(r'^send_campaign/$', send_campaign),
-      url(r'^schedule_next_newsletter/$', schedule_next_newsletter),
+
 
 )
 
